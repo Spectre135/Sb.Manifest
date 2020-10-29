0b29fa2cfbced3463e2ce36ec999fb94415fdf68
Spectre135 <matej.batistic@gmail.com>
2020-10-29 15:24:13 +0100

    a

--- sb.manifest.web/app/components/load/loadCtrl.js
+++ sb.manifest.web/app/components/load/loadCtrl.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('SbManifest');
 
-app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog, $mdSidenav, apiService, config) {
+app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter,$window, $mdDialog, $mdSidenav, apiService, config) {
 
     $scope.loads;
     $scope.query = {
@@ -24,15 +24,16 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
         var url = config.manifestApi + '/load/list';
         var promise = apiService.getData(url, params, true)
             .then(function (data) {
-                $scope.loads = data.DataList;
-                getProducts();
+                $scope.loads = data.DataList;        
             });
-        return promise;
+        return promise;    
     };
 
     //init
     $scope.init = function () {
-        $scope.getLoadList();
+        $scope.getLoadList().then(function () {
+            getProducts();
+        });
     };
 
     function getProducts() {
@@ -50,7 +51,7 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
 
     //add people to load
     $scope.addPeople = function ($event, dto, idProduct) {
-        dto.IdProductSelected=idProduct;
+        dto.IdProductSelected = idProduct;
         $mdDialog.show({
             locals: {
                 dataToPass: dto
@@ -203,8 +204,9 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
                 .then(function () {
                     $scope.moved = {};
                     $scope.moved.IdCustomer = [];
-                    $scope.getLoadList();
-                    setTimeout(reselectCustomer, 500);
+                    $scope.getLoadList().then(function () {
+                        reselectCustomer();
+                    });
                 });
         } else {
             var url = config.manifestApi + '/load/slot/move';
@@ -214,18 +216,22 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
                     $scope.moved = {};
                     $scope.moved.IdCustomer = [];
                     //we must refresh load list
-                    $scope.getLoadList();
-                    setTimeout(reselectCustomer, 500);
+                    $scope.getLoadList().then(function () {
+                        reselectCustomer();
+                    });
                 });
         }
     };
 
     function reselectCustomer() {
+        console.log($scope.selectedIdCustomer);
         if ($scope.selectedIdCustomer > -1) {
-            var slots = angular.element('.load .slot[data-idcustomer="' + $scope.selectedIdCustomer + '"]');
-            slots.addClass('selected');
+            setTimeout(function () {
+                var slots = angular.element('.load .slot[data-idcustomer="' + $scope.selectedIdCustomer + '"]');
+                slots.addClass('selected');
+            }, 500);
         }
-    }
+    };
 
     $scope.openSideMenu = function () {
         getActiveToday();
@@ -282,8 +288,7 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
             var slots = angular.element('.load .slot[data-idcustomer="' + IdCustomer + '"]');
             slots.removeClass('selected');
             $scope.selectedIdCustomer = -1;
-        }
-        else {
+        } else {
             var slots = angular.element('.load .slot.selected');
             slots.removeClass('selected');
             slots = angular.element('.load .slot[data-idcustomer="' + IdCustomer + '"]');
@@ -291,6 +296,7 @@ app.controller('loadCtrl', function ($rootScope, $scope, $q, $filter, $mdDialog,
             $scope.selectedIdCustomer = IdCustomer;
         }
     };
+
 });
 
 app.controller('confirmLoadCtrl', function ($scope, $state, $filter, $mdDialog, $window, dataToPass, apiService, config) {
