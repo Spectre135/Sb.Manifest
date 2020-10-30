d21e8c0df60a44ecafd7e0bc493cd0eed83cbfab
sasotepina <saso.tepina@logos.si>
2020-10-31 00:25:16 +0100

    popravljeni reporti, popravljene settings tabele

--- sb.manifest.web/app/components/reports/reportCtrl.js
+++ sb.manifest.web/app/components/reports/reportCtrl.js
@@ -3,7 +3,7 @@
 var app = angular.module('SbManifest');
 
 app.controller('reportCtrl', function ($scope, $state, $window, apiService, config) {
-    $scope.loads = 0; 
+    $scope.loads = 0;
     $scope.profit = 0;
 
     //get dashboard product list data
@@ -15,25 +15,25 @@ app.controller('reportCtrl', function ($scope, $state, $window, apiService, conf
                 $scope.productList = data.DataList;
                 //Loads
                 var chart1 = getDoughnutChart($scope.productList, 'Product', 'BackgroundColor', 'Number', 'Jumps');
-                var ctx1 = document.getElementById('loadsChart').getContext('2d');
+                var ctx1 = angular.element('#loadsChart')[0].getContext('2d');
                 var chart1 = new Chart(ctx1, chart1);
                 //Profit load
                 var chart2 = getDoughnutChart($scope.productList, 'Product', 'BackgroundColor', 'Profit', 'Income');
-                var ctx2 = document.getElementById('profitChart').getContext('2d');
+                var ctx2 = angular.element('#profitChart')[0].getContext('2d');
                 var chart2 = new Chart(ctx2, chart2);
                 sum();
             });
     };
 
-    function sum(){
+    function sum() {
         angular.forEach($scope.productList, function (value, key) {
-            $scope.loads = $scope.loads+ value.Number; 
-            $scope.profit = $scope.profit+ value.Profit;
+            $scope.loads = $scope.loads + value.Number;
+            $scope.profit = $scope.profit + value.Profit;
         });
     };
 
-    //line chart
-  
+    // line chart
+
     var meseci = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var labels1 = [0, 10, 30, 130, 1300, 2345, 1800, 1950, 1500, 855, 0, 0];
     var labels2 = [0, 910, 930, 4130, 4300, 8345, 7800, 6950, 4500, 3955, 0, 0];
