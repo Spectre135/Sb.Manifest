043415f74e17d425ff0679d493d775af4ab154ed
Spectre135 <matej.batistic@gmail.com>
2020-11-09 15:15:55 +0100

    signalr za loade <15min

--- sb.manifest.api/Services/HubService.cs
+++ sb.manifest.api/Services/HubService.cs
@@ -1,27 +1,43 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿#region using
+using Microsoft.AspNetCore.SignalR;
 using sb.manifest.api.Hubs;
 using sb.manifest.api.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
+#endregion
 
 namespace sb.manifest.api.Services
 {
     public static class HubService
     {
         private static readonly DisplayHub displayHub = new DisplayHub();
+        private static readonly AlertHub alertHub = new AlertHub();
         public static void SendLoadList(MResponse mResponse, IHubContext<DisplayHub> hubContext)
         {
             try
             {
-                var task = Task.Run(async () => await displayHub.Send(mResponse, hubContext));
-            }catch (Exception ex)
+                //var task = Task.Run(async () => await displayHub.Send(mResponse, hubContext));
+                Thread t = new Thread(async () => await displayHub.Send(mResponse, hubContext));
+                t.Start();
+            }
+            catch (Exception ex)
             {
                 //TODOD logger
             }
             
         }
+        public static void SendAlert(MResponse mResponse, IHubContext<AlertHub> hubContext)
+        {
+            try
+            {
+                Thread t = new Thread(async () => await alertHub.Send(mResponse, hubContext));
+                t.Start();
+            }
+            catch (Exception ex)
+            {
+                //TODOD logger
+            }
 
+        }
     }
 }
---
+++ sb.manifest.api/Tasks/LoadAlertBackgroundTask.cs
@@ -0,0 +1,60 @@
+﻿#region using
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using sb.manifest.api.DAO;
+using sb.manifest.api.Hubs;
+using sb.manifest.api.Model;
+using sb.manifest.api.Services;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+#endregion
+
+namespace sb.manifest.api.Tasks
+{
+    public class LoadAlertBackgroundTask : IHostedService, IDisposable
+    {
+        private Timer timer;
+        private readonly IConfiguration config;
+        private readonly IHubContext<AlertHub> hubContext;
+
+        public LoadAlertBackgroundTask(IConfiguration configuration, IHubContext<AlertHub> _hubContext)
+        {
+            config = configuration;
+            hubContext = _hubContext;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer = new Timer(o => {
+                SendAlert();
+            },
+            null,
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(60));
+
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        private void SendAlert()
+        {
+            MResponse mResponse = new MResponse();
+
+            using LoadDAO dao = new LoadDAO();
+                mResponse= dao.GetLoads(config,0,true);
+
+            Console.WriteLine(mResponse.RowsCount);
+
+            HubService.SendAlert(mResponse, hubContext);
+        } 
+    }
+
+}
