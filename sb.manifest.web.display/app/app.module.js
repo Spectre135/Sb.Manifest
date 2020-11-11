54072fe17718dc1709583feb0b3b0d566a09221c
Spectre135 <matej.batistic@gmail.com>
2020-11-11 13:47:53 +0100

    web display date from server

--- sb.manifest.web.display/app/app.module.js
+++ sb.manifest.web.display/app/app.module.js
@@ -7,7 +7,28 @@ var app = angular.module('SbManifest', [
     'ngMessages'
 ]);
 
-app.run(function ($state, $rootScope,$mdDialog) {
+app.run(function ($state, $rootScope, $mdDialog, apiService, config) {
+
+    //load server time
+    apiService.getData(config.manifestApi + '/server/time', null, false).then(function (data) {
+        $rootScope.newDateServerTime = new Date(data) - new Date();
+        console.log('Server time ' + new Date(data));
+        console.log('Time diff from server in miliseconds ' + $rootScope.newDateServerTime);
+    });
+
+    //to get Date from server
+    $rootScope.getDate = function () {
+        var copiedDate = new Date();
+        return new Date(copiedDate.getTime() + $rootScope.newDateServerTime);
+    };
+    //calculate diff time from server and client
+    $rootScope.getTimeDiffInMInutes = function (date) {
+        var copiedDate = new Date();
+        var now = new Date(copiedDate.getTime() + $rootScope.newDateServerTime);
+        var scheduled = new Date(date);
+        var diff = scheduled - now;
+        return Math.floor(diff / 60000);
+    };
 
     window.myAppErrorLog = [];
 
@@ -16,18 +37,18 @@ app.run(function ($state, $rootScope,$mdDialog) {
         window.myAppErrorLog.push(error);
     });
 
-        //alert dialog
-        $rootScope.showDialog = function (title, message) {
-            $mdDialog.show(
-                $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#main')))
-                .clickOutsideToClose(true)
-                .title(title)
-                .textContent(message)
-                .ariaLabel('Alert Dialog')
-                .ok('close')
-            );
-        };
+    //alert dialog
+    $rootScope.showDialog = function (title, message) {
+        $mdDialog.show(
+            $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#main')))
+            .clickOutsideToClose(true)
+            .title(title)
+            .textContent(message)
+            .ariaLabel('Alert Dialog')
+            .ok('close')
+        );
+    };
 
     //we check if user is authenticated
     if (sessionStorage.getItem('Authorization') === null) {
